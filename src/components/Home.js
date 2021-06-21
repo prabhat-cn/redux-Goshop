08603769d1173177c3f6ee2e48dad2392a07cd51
gitcoder-23 <prabhatece21@gmail.com>
2021-06-21 08:29:09 +0530

    🐛

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ const Range = createSliderWithTooltip(Slider.Range)
 
 const Home = ({ match }) => {
   const [currentPage, setCurrentPage] = useState(1)
-  const [price, setPrice] = useState([1,1000]);
+  const [price, setPrice] = useState([1,1000])
 
   const alert = useAlert();
   const dispatch = useDispatch();
